b38c58a909d3815fe58a3648b4d1532add056c9b
igorcsouzaa <igorcs1104@gmail.com>
2025-11-13 20:47:33 -0300

    update background

--- SRC/static/script.js
+++ SRC/static/script.js
@@ -158,3 +158,22 @@ const navMenu = document.getElementById("nav-menu");
   });
 
 
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const starBackground = document.querySelector(".star-background");
+  const numStars = 150;
+
+  for (let i = 0; i < numStars; i++) {
+    const star = document.createElement("div");
+    star.classList.add("star");
+    const size = Math.random() * 3 + 1;
+    star.style.width = `${size}px`;
+    star.style.height = `${size}px`;
+    star.style.top = `${Math.random() * 100}vh`;
+    star.style.left = `${Math.random() * 100}vw`;
+    star.style.animationDuration = `${Math.random() * 5 + 5}s`;
+    star.style.animationDelay = `${Math.random() * 5}s`;
+    starBackground.appendChild(star);
+  }
+});
